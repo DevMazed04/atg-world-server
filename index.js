4cdb3a46a5b2ae3e86fa2bc18f1a8168c819ced3
DevMazed04 <mdmazedislam2684@gmail.com>
2023-02-03 01:11:33 +0600

    created posts related API's

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ app.get('/', (req, res) => {
    res.send('ATG World API is running');
 })
 
+app.get('/posts', (req, res) => {
+   res.send(posts);
+})
+
+app.get('/article', (req, res) => {
+   const articles = posts.filter(article => article.category.name === "Article");
+   res.send(articles);
+})
+
+app.get('/event', (req, res) => {
+   const events = posts.filter(event => event.category.name === "Event");
+   res.send(events);
+})
+
+app.get('/education', (req, res) => {
+   const education = posts.filter(edu => edu.category.name === "Education");
+   res.send(education);
+})
+
+app.get('/job', (req, res) => {
+   const jobs = posts.filter(job => job.category.name === "Job");
+   res.send(jobs);
+})
 
 app.listen(port, () => {
    console.log('ATG World server is running on port', port)
