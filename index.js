8dc09d300a1781024b9d356174c0b123d2115c4e
DevMazed04 <mdmazedislam2684@gmail.com>
2023-03-13 14:45:31 +0600

    index.js updated

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middlewares
 app.use(cors());
 
 const categories = require('./data/categories.json');
 const posts = require('./data/posts.json');
 const groups = require('./data/groups.json');
 
+
+// api's
 app.get('/', (req, res) => {
    res.send('ATG World API is running');
 })
